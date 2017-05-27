2b9c2c4791950bc132238362c178c65a87bc855d
SirBarbier <matthew-7.7@hotmail.com>
2017-05-27 13:45:09 +0100

    Initial check in
    Basic working web api demo

---
+++ web_api_test/web_api_test/Controllers/GamesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace web_api_test.Controllers
+{
+    public class GamesController : ApiController
+    {
+        Models.Games[] gamesList = new Models.Games[] {
+            new Models.Games { ID = 1, Name = "GalCiv3", GameGenre = Models.Genre.FourX, Rating = 5},
+            new Models.Games { ID = 2, Name = "FarCry3", GameGenre = Models.Genre.FPS, Rating = 8},
+            new Models.Games { ID = 3, Name = "Atilla Total War", GameGenre = Models.Genre.GrandStrategy, Rating = 9},
+        };
+        
+        public IEnumerable<Models.Games> GetAllGames()
+        {
+            return gamesList;
+        }
+
+        public IHttpActionResult GetGame(int id)
+        {
+            var game = gamesList.FirstOrDefault(g => g.ID == id);
+
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(game);
+        }
+    }
+}
---
+++ web_api_test/web_api_test/Models/Games.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web_api_test.Models
+{
+    public class Games
+    {
+        public string Name { get; set; }
+        public int Rating { get; set; }
+        public Genre GameGenre { get; set; }
+        public int ID { get; set; }
+
+        public Games()
+        {
+
+        }
+
+        public Games(int id, string name, int rating, Genre genre)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Rating = rating;
+            this.GameGenre = genre;
+        }
+    }
+
+    public enum Genre
+    {
+        RPG, RTS, FourX, GrandStrategy, FPS, Roguelike
+    }
+}
